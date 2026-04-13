5ebb012ae2473880b9e79886acd499e57163e32e
Hunter Boyd <hboyd5310@gmail.com>
2026-04-13 08:17:18 -0700

    Update AddBreadWhenClicked.cs

--- BreadStackeR - Hunter B/Assets/Scripts/AddBreadWhenClicked.cs
+++ BreadStackeR - Hunter B/Assets/Scripts/AddBreadWhenClicked.cs
@@ -1,46 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine;
 
+/// <summary>
+/// Clickable shelf object that gives bread to the player.
+/// </summary>
 public class AddBreadWhenClicked : MonoBehaviour
 {
-    public int breadCount = 5;              // Bread available on this shelf
-    public PlayerInventory playerInventory; // Reference to player inventory
+    [Header("Shelf Settings")]
+    [SerializeField] private int breadCount = 5;
 
-    void Start()
+    [Header("References")]
+    [SerializeField] private PlayerInventory playerInventory;
+
+    private void Start()
     {
-        // Auto-find PlayerInventory if not assigned
+        // Auto-find PlayerInventory if it is not assigned.
         if (playerInventory == null)
         {
             playerInventory = FindObjectOfType<PlayerInventory>();
         }
     }
 
-    // Called when object is clicked
-    void OnMouseDown()
+    private void OnMouseDown()
     {
-        if (breadCount > 0)
+        if (breadCount <= 0)
         {
-            breadCount--;
+            Debug.Log("Shelf is empty!");
+            return;
+        }
 
-            // Add bread to player
-            if (playerInventory != null)
-            {
-                playerInventory.AddBread();
-            }
+        breadCount--;
 
-            Debug.Log(gameObject.name + " bread left: " + breadCount);
-
-            // Notify when empty
-            if (breadCount == 0)
-            {
-                Debug.Log(gameObject.name + " is empty!");
-            }
+        if (playerInventory != null)
+        {
+            playerInventory.AddBread(1);
         }
         else
         {
-            Debug.Log("Shelf is empty!");
+            Debug.LogWarning("PlayerInventory reference is missing.");
+        }
+
+        Debug.Log($"{gameObject.name} bread left: {breadCount}");
+
+        if (breadCount == 0)
+        {
+            Debug.Log($"{gameObject.name} is empty!");
         }
     }
 }
