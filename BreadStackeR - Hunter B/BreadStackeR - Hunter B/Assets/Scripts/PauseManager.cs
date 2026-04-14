909324dbc233d17174f526e851c77a2584931dd0
Hunter Boyd <hboyd5310@gmail.com>
2026-04-13 21:45:10 -0700

    Update Main Warehouse scene UI & PauseManager
    Modify Main Warehouse.unity to add and reconfigure several UI elements: new TMP text ("Pretty Sure."), buttons (ResumeButton, PrettySureButton, SettingsButton / NaBRO), a Slider, Background and Handle Slide Area objects, and CanvasRenderers/RectTransforms for those items. UI hierarchy and RectTransform anchors/positions were adjusted and some GameObjects were activated. Button OnClick targets were wired to PauseManager methods (Resume, ConfirmQuit, CancelQuit). PauseManager.cs was updated accordingly to support the new UI interactions.

--- BreadStackeR - Hunter B/BreadStackeR - Hunter B/Assets/Scripts/PauseManager.cs
+++ BreadStackeR - Hunter B/BreadStackeR - Hunter B/Assets/Scripts/PauseManager.cs
@@ -1,14 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
     
     public GameObject pauseCanvas;
     public GameObject alwaysOnCanvas;
+    public GameObject settingsCanvas;
+    public GameObject confirmationPanel;
 
     private bool isPaused;
+
+    public void Resume() //resume game
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        pauseCanvas.SetActive(false);
+        alwaysOnCanvas.SetActive(true);
+    }
+
+    public void Settings() //settings menu
+    {
+        settingsCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+    
+    public void Quit() //quit to main menu
+    {
+        confirmationPanel.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+    
+    public void CancelQuit() //cancel quit to main menu
+    {
+        confirmationPanel.SetActive(false);
+        pauseCanvas.SetActive(true);
+    }
+
+    public void ConfirmQuit()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
    
     // Update is called once per frame
     void Update()
